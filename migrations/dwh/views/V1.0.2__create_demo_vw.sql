f99e9a369d3cc6bde99246a6731eab32d424da4c
AvinashPandey1999 <avinash.m.pandey@kipi.bi>
2025-01-29 00:08:00 +0530

    Updated

--- migrations/dwh/views/V1.0.2__create_demo_vw.sql
+++
@@ -1,6 +0,0 @@
--- Demo view for Dev_rep db under sf schema
-
--- Create a View
-CREATE IF NOT EXISTS VIEW {{database_name}}_DWH.SF.SAMPLE_VIEW AS
-SELECT id, name
-FROM {{database_name}}_REP.SF.SAMPLE_TABLE;
