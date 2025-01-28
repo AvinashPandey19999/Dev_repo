1c605738620ce93932116d3b059149766de8b038
AvinashPandey1999 <avinash.m.pandey@kipi.bi>
2025-01-28 23:56:16 +0530

    Initial Commit for Table, view and Sp

---
+++ migrations/rep/tables/V1.0.0__create_demo_tb.sql
@@ -0,0 +1,13 @@
+-- Demo table for Dev_rep db under sf schema
+
+-- Create a Table
+CREATE TABLE IF NOT EXISTS {{database_name}}_REP.SF.SAMPLE_TABLE (
+    ID INT,
+    NAME STRING
+);
+
+-- Insert Data into the Table
+INSERT INTO {{database_name}}_REP.SF.SAMPLE_TABLE (ID, NAME)
+VALUES (1, 'Alice'),
+       (2, 'Bob'),
+       (3, 'Charlie');
